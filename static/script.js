b456729439ff589c644e19f4a080d935c1b796d3
Getkey <getkey@getkey.eu>
2016-11-13 00:13:47 +0100

    Do AJAX requests
    It works!

--- static/script.js
+++ static/script.js
@@ -3,11 +3,29 @@
 let mv = new Vue({
 	el: '#app',
 	data: {
-		message: 'Hello Vue!'
+		currentCountry: null,
+		stereotypes: [],
+		fetched: false
 	},
 	methods: {
-		displayCountryCode: function(ev) {
-			if (ev.target.tagName === "path") console.log(ev.target.id);
+		displayStereotypes: function(ev) {
+			if (ev.target.tagName === 'path') {
+				this.currentCountry = ev.target.getAttribute('title');
+				var req = new XMLHttpRequest();
+				req.open('GET', '/api/' + ev.target.id + '.json', true);
+				req.addEventListener('load', function(ev) {
+					this.stereotypes = JSON.parse(ev.target.responseText);
+					this.fetched = true;
+				}.bind(this));
+				req.send(null);
+			}
+		},
+		hideStereotypes: function(ev) {
+			if (ev.target.tagName === 'path') {
+				this.currentCountry = null;
+				this.stereotypes = [];
+				this.fetched = false;
+			}
 		}
 	}
 });
